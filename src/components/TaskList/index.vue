325e5e1604d3501418fa3a52f9176d8d406b23ad
Vitor Duggen <zduggem@gmail.com>
2022-05-25 17:08:50 -0300

    Refotarado o taskList separado em um componente

--- src/components/TaskList/index.vue
+++ src/components/TaskList/index.vue
@@ -1,86 +1,34 @@
 <template>
-  <div class="list-items">
-    <template v-if="loading">
-      <div
-        v-for="n in 6"
-        :key="n"
-        class="loading-item"
-      >
-        <span class="glow-checkbox" />
-
-        <span class="glow-text">
-          <span>Loading</span>
-          <span>cool</span>
-          <span>state</span>
-        </span>
-     </div>
-    </template>
-
-    <template
-      v-else-if="isEmpty"
-      class="list-items"
-    >
-      <div class="wrapper-message">
-        <span class="icon-check" />
-        <div class="title-message">You have no tasks</div>
-        <div class="subtitle-message">Sit back and relax</div>
-      </div>
-    </template>
-
-    <template v-else>
-      <Task
-        v-for="task in tasksInOrder"
-        :key="task.id"
-        :task="task"
-        @archive-task="onArchiveTask"
-        @pin-task="onPinTask"
-      />
-    </template>
-  </div>
+  <PureTaskList
+    :tasks="allTasks"
+    @archive-task="archiveTask"
+    @pin-task="pinTask"
+  />
 </template>
 
 <script>
-import Task from "../Task/index";
+import { mapGetters } from "vuex";
+import { ARCHIVE_TASK, PIN_TASK } from "../../store/types";
+import PureTaskList from "../PureTaskList/index";
 
 export default {
   name: "TaskList",
   
   components: {
-    Task
+    PureTaskList
   },
 
-  props: {
-    tasks: {
-      type: Array,
-      required: true,
-      default: () => []
-    },
-    loading: {
-      type: Boolean,
-      default: false
-    }
-  },
-
-  computed: { 
-    isEmpty() {
-      return this.tasks.length === 0;
-    },
-
-    tasksInOrder() {
-      return [
-        ...this.tasks.filter(t => t.state === "TASK_PINNED"),
-        ...this.tasks.filter(t => t.state !== "TASK_PINNED"),
-      ]
-    }
+  computed: {
+    ...mapGetters(['allTasks'])
   },
 
   methods: {
-    onArchiveTask(taskId) {
-        this.$emit('archive-task', taskId);
+    archiveTask(task) {
+      this.$store.dispatch(ARCHIVE_TASK.GET, task);
     },
 
-    onPinTask(taskId) {
-        this.$emit('pin-task', taskId);
+    pinTask(task) {
+      this.$store.dispatch(PIN_TASK.GET, task);
     }
   }
 }
